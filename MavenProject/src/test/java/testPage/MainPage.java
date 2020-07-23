e9b26c8d6a2eefe3a0621dbba01bf6971380b305
Jonathan Tolentino <jt.prototype20@gmail.com>
2020-07-22 23:03:47 -0700

    updated

--- MavenProject/src/test/java/testPage/MainPage.java
+++ MavenProject/src/test/java/testPage/MainPage.java
@@ -1,9 +1,6 @@
 package testPage;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import pageObject.MainPageBase;
 
 public class MainPage extends MainPageBase{
