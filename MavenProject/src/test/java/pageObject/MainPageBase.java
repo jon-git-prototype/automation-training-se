199861d99fba0dc74cde804010c988969f3e6c5a
Jonathan Tolentino <jt.prototype20@gmail.com>
2020-07-22 22:03:46 -0700

    updated

--- MavenProject/src/test/java/pageObject/MainPageBase.java
+++ MavenProject/src/test/java/pageObject/MainPageBase.java
@@ -7,14 +7,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class MainPageBase {
+	
+	public static WebDriver driver;
 
 	public void OpenBrowser(String URL) {
 		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(URL);
-				
+		driver.get(URL);			
+	}
+	
+	public void CloseBrowser() {
+		
+		driver.quit();
 	}
 	
 }
