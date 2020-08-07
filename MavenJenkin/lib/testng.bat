74524be11961450341996e0f259bb6cc9c5a4adc
JT <jt.prototype20@gmail.com>
2020-08-07 16:14:40 +0800

    update

--- MavenJenkin/lib/testng.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Test Automation Training\Training Projects\automation-training-se\MavenJenkin
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
