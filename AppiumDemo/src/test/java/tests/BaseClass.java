34e17a8d2a3ee3d6724606b1ccd3b8e5cbaa7b23
JT <jt.prototype20@gmail.com>
2020-08-12 13:57:47 +0800

    update

--- AppiumDemo/src/test/java/tests/BaseClass.java
+++ AppiumDemo/src/test/java/tests/BaseClass.java
@@ -14,8 +14,8 @@ public class BaseClass {
 		//caps.setCapability("platformName", "ANDROID");
 		//caps.setCapability(CapabilityType.PLATFORM_NAME, "ANDROID");
 		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "ANDROID");
-		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "");
-		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "");
+		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.2.2");
+		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Emulator2");
 		caps.setCapability(MobileCapabilityType.UDID, "");
 		caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
 		caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
--- AppiumDemo/src/test/java/tests/CalculatorDemo.java
+++ AppiumDemo/src/test/java/tests/CalculatorDemo.java
@@ -2,13 +2,18 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
- 
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 /**
@@ -16,17 +21,18 @@ import java.net.URL;
  */
 public class CalculatorDemo {
  
-    WebDriver driver;
+    static AppiumDriver<MobileElement> driver;
  
     @BeforeClass
     public void setUp() throws MalformedURLException{
         //Set up desired capabilities and pass the Android app-activity
         //and app-package to Appium
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("BROWSER_NAME", "Android");
-        capabilities.setCapability("VERSION", "4.4.2");
-        capabilities.setCapability("deviceName","EmulatorS4");
+        //capabilities.setCapability("BROWSER_NAME", "Emulator2");
+        capabilities.setCapability("deviceName","Emulator3");
+        capabilities.setCapability("UDID", "emulator-5554");
         capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("platformVersion", "9");
  
         // This package name of your app (you can get it from apk info app)
         capabilities.setCapability("appPackage", "com.android.calculator2");
@@ -37,30 +43,35 @@ public class CalculatorDemo {
         //Create RemoteWebDriver instance and connect to the Appium server
         //It will launch the Calculator App in Android Device using the configurations
         //specified in Desired Capabilities
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
+        
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        driver = new AppiumDriver<MobileElement>(url,capabilities);
+       // driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+   }
  
-    @Test
-    public void testCal() throws Exception {
+  @Test
+   public void testCal() throws Exception {
         //locate the Text on the calculator by using By.name()
-        WebElement two=driver.findElement(By.name("2"));
-        two.click();
-        WebElement plus=driver.findElement(By.name("+"));
-        plus.click();
-        WebElement four=driver.findElement(By.name("4"));
+        MobileElement two = driver.findElement(By.id("com.android.calculator2:id/digit_2"));
+        two.click(); 
+        MobileElement plus = driver.findElement(By.id("com.android.calculator2:id/op_add"));
+        plus.click();   
+        MobileElement four = driver.findElement(By.id("com.android.calculator2:id/digit_4"));
         four.click();
-        WebElement equalTo=driver.findElement(By.name("="));
-        equalTo.click();
+        MobileElement equalTo = driver.findElement(By.id("com.android.calculator2:id/eq"));
+        equalTo.click();   
         //locate the edit box of the calculator by using By.tagName()
-        WebElement results=driver.findElement(By.className("android.widget.EditText"));
+        //MobileElement results=driver.findElement(By.id("com.android.calculator2:id/formula"));
+        MobileElement results1=driver.findElement(By.id("com.android.calculator2:id/result"));
+        
         //Check the calculated value on the edit box
-        assert results.getText().toString().equals("6"):"Actual value is : "
-                +results.getText()+" did not match with expected value: 6";
+        assert results1.getText().toString().equals("6"):"Actual value is : "
+                +results1.getText()+" did not match with expected value: 6";
     }
  
-    @AfterClass
+ /*   @AfterClass
     public void teardown(){
         //close the app
         driver.quit();
-    }
+    } */
 }
